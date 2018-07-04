b603407b83e132453b3ca147360676cdb939c106
Čedomir Krsmanović <30358338+krsmanovic@users.noreply.github.com>
2018-07-04 10:26:31 +0200

    Update block-stretchoid-shodan.sh

--- home/scripts/block-stretchoid-shodan.sh
+++ home/scripts/block-stretchoid-shodan.sh
@@ -2,13 +2,13 @@
 
 # Created for MikroTik routers with existing firewall facility.
 # Aim is to form permanent block list for scanning farms that
-# have known PTR entries.
-# 						Che, July 2018
+# have known PTR records.
+#                                               Che, July 2018
 
 # Create ssh key pair for your router and use
 # those credentials for logging in
-router_ip="192.168.0.1"
-router_username="admin"
+router_ip="62.193.139.170"
+router_username="che"
 router_port="22"
 # Stage 1 dynamic address list on the remote MikroTik
 router_stage1_dynamic="vpn_stage1"
@@ -25,16 +25,16 @@ ssh $router_username@$router_ip -p $router_port "/ip firewall address-list print
 while IFS='' read -r ip || [[ -n "$ip" ]]
 do 
 
-	ip=$(echo $ip | tr -d '\r')
-	domain=$(dig -x "$ip" +short | sed 's/.$//')
+  ip=$(echo $ip | tr -d '\r')
+  domain=$(dig -x "$ip" +short | sed 's/.$//')
 
-	if [ ! -z "$domain" ]
-	then
-		if echo $domain | egrep -i "(^|[^a-zA-Z])(stretchoid|shodan)($|[^a-zA-Z])" > /dev/null
-			then
-		        ssh $router_username@$router_ip -p $router_port "do { /ip firewall address-list add list=$router_blacklist comment=$domain address=$ip } on-error={}"
-		        echo "Host $ip with PTR $domain was added to the blacklist on the remote device."
-		fi
-	fi
+  if [ ! -z "$domain" ]
+  then
+    if echo $domain | egrep -i "(^|[^a-zA-Z])(stretchoid|shodan)($|[^a-zA-Z])" > /dev/null
+      then
+        ssh $router_username@$router_ip -p $router_port "do { /ip firewall address-list add list=$router_blacklist comment=$domain address=$ip } on-error={}"
+        echo "Host $ip with PTR $domain was added to the blacklist on the remote device."
+    fi
+  fi
 
 done < "$file"
