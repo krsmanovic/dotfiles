bbeb5267a39abddf53ae669aa76b96399696abca
krsmanovic <cedo.krsmanovic@gmail.com>
2020-12-28 14:28:55 +0100

    Added default value for public key location.

--- windows/_Scripts/ssh-copy-id.ps1
+++ windows/_Scripts/ssh-copy-id.ps1
@@ -1,4 +1,9 @@
-$keyFilePath = Read-Host "Please enter full path to public key file"
-$sshServer = Read-Host "Please enter the ssh server in format user@hostname or user@ip"
+$keyFilePath = Read-Host "Please enter full path to public key file (leave blank for default: C:\Users\$env:USERNAME\.ssh\id_rsa.pub)"
+
+if ([string]::IsNullOrWhiteSpace($keyFilePath)) {
+    $keyFilePath = "C:\Users\$env:USERNAME\.ssh\id_rsa.pub"
+}
+
+$sshServer = Read-Host "Please enter the ssh server (user@hostname or user@ip)"
 
 type $keyFilePath | ssh $sshServer "cat >> .ssh/authorized_keys"
