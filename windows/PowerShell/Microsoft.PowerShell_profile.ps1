0edd47e23060a5a6fa6ab8be725624133e610783
krsmanovic <cedo.krsmanovic@gmail.com>
2021-06-12 21:19:13 +0200

    Automatically copy newly generated GUID to clipboard.

--- windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,8 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Agnoster
 
-function new-guid {
-    [guid]::NewGuid()   
+function New-Guid {
+    Set-Variable -Name "guidValue" -Value ([guid]::NewGuid())
+    Set-Clipboard $guidValue
+    Write-Output "`nUnique GUID is generated and copied to clipboard.`n`nThe value is:`n$guidValue`n"
 }
