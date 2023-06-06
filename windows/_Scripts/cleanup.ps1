e80d113e39f2f140a6178825da8f7828390b4434
krsmanovic <cedo.krsmanovic@gmail.com>
2023-06-06 08:39:01 +0200

    [windows] Add cleanup script and scheduled task

---
+++ windows/_Scripts/cleanup.ps1
@@ -0,0 +1,19 @@
+Get-Service -Name wuauserv | Stop-Service -Force -verbose -ErrorAction SilentlyContinue
+
+$folders = @(
+    'j:\download'
+    'C:\Windows\SoftwareDistribution'
+    'C:\Windows\Temp'
+    'C:\Windows\prefetch'
+    'C:\inetpub\logs\LogFiles'
+    'C:\Users\*\Downloads'
+    'C:\Users\*\AppData\Local\Temp'
+    'C:\Users\*\AppData\Local\Microsoft\Windows\Temporary Internet Files'
+    'C:\Users\*\AppData\Local\Microsoft\Windows\History'
+    'C:\Users\*\AppData\Local\Microsoft\Windows\INetCookies'
+)
+foreach($f in $folders) {
+    Get-ChildItem -Path "$f\*" -Recurse -Force -ErrorAction SilentlyContinue | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -Confirm:$false
+}
+
+Get-Service -Name wuauserv | Start-Service -Verbose
