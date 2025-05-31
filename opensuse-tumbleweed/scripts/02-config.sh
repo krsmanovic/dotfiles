ff5d9a7ba1a50c50d8961f334e3e8712d1b74086
krsmanovic <cedo.krsmanovic@gmail.com>
2025-05-31 13:47:18 +0200

    [cfg] change smb setup

--- opensuse-tumbleweed/scripts/02-config.sh
+++ opensuse-tumbleweed/scripts/02-config.sh
@@ -2,11 +2,15 @@
 
 # vars
 DESKTOP_USER=che
-SMB_CONFIG_DIR="/home/$DESKTOP_USER/.config/smb"
-SMB_CREDENTIALS_FILE=".credentials"
+CREDENTIALS_DIR=/home/$DESKTOP_USER/.credentials
+SMB_CREDENTIALS_FILE="smb"
 SMB_SHARE_PATH="//smb.lan/cher"
 SMB_MOUNT_DIR="/mnt/smb"
+SMB_FSTAB_LINE="$SMB_SHARE_PATH $SMB_MOUNT_DIR cifs credentials=$CREDENTIALS_DIR/$SMB_CREDENTIALS_FILE 0 0"
 NETWORK_MANAGER_CONFIG_OVERRIDES_PATH="/etc/NetworkManager/conf.d/99-overrides.conf"
+
+# setup directories
+mkdir -p $CREDENTIALS_DIR
 
 # load common functions
 source /home/$DESKTOP_USER/lib/sh/common.sh
@@ -18,20 +22,9 @@ sudo sed -i "s/^myhostname =.*/myhostname = $REAL_HOSTNAME/" /etc/postfix/main.c
 
 # set smb share
 log_message info "Setting up smb..."
-sudo mkdir "$SMB_MOUNT_DIR"
-mkdir -p "$SMB_CONFIG_DIR"
-echo "Please create SMB credentials file at location $SMB_CONFIG_DIR/$SMB_CREDENTIALS_FILE."
-read -p "Have you created SMB credentials file? y/n " -n 1 -r
-echo
-if [ $REPLY =~ ^[Yy]$ ]; then
-    log_message info "SMB credentials file was created. Continuing..."
-else
-    log_message warning "No SMB credentials file was created. Exiting..."
-    exit 0
-fi
-if [ -f $SMB_CONFIG_DIR/$SMB_CREDENTIALS_FILE ]; then
-    SMB_USER="$(grep username $SMB_CONFIG_DIR/$SMB_CREDENTIALS_FILE | awk -F'=' '{print $2}')"
-    SMB_PASSWORD="$(grep password $SMB_CONFIG_DIR/$SMB_CREDENTIALS_FILE | awk -F'=' '{print $2}')"
+if [ -f $CREDENTIALS_DIR/$SMB_CREDENTIALS_FILE ]; then
+    SMB_USER="$(grep username $CREDENTIALS_DIR/$SMB_CREDENTIALS_FILE | awk -F'=' '{print $2}')"
+    SMB_PASSWORD="$(grep password $CREDENTIALS_DIR/$SMB_CREDENTIALS_FILE | awk -F'=' '{print $2}')"
     if [ -z "${SMB_USER}" ] || [ -z "${SMB_PASSWORD}" ]; then
         log_message err "SMB credentials file must contain both username and password values. Exiting..."
         exit 1
@@ -40,8 +33,12 @@ else
     log_message err "No SMB credentials file found. Exiting..."
     exit 1
 fi
-echo "$SMB_SHARE_PATH $SMB_MOUNT_DIR cifs credentials=$SMB_CONFIG_DIR/$SMB_CREDENTIALS_FILE 0 0" | sudo tee --append /etc/fstab
-echo "//smb.lan/cher /mnt/smb cifs credentials=/home/che/.config/smb/.credentials 0 0" | sudo tee --append /etc/fstab
+if grep "$SMB_FSTAB_LINE" /etc/fstab; then
+    log_message info "SMB configuration is already present in fstab."
+else
+    log_message info "Adding SMB configuration to fstab..."
+    sudo tee --append /etc/fstab > /dev/null <<< "$SMB_FSTAB_LINE"
+fi
 
 # network manager overrides
 sudo tee $NETWORK_MANAGER_CONFIG_OVERRIDES_PATH > /dev/null << EOF
