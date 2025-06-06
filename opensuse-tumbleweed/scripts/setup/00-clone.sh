bc9be21b029681ab3c44a471a7c3dc5d7fa487fb
krsmanovic <cedo.krsmanovic@gmail.com>
2025-06-06 21:28:05 +0200

    [add] allow bootstrap script in sudoers

--- opensuse-tumbleweed/scripts/setup/00-clone.sh
+++ opensuse-tumbleweed/scripts/setup/00-clone.sh
@@ -3,6 +3,8 @@
 # vars
 DESKTOP_USER=che
 WORKDIR="$(mktemp -d)"
+SUDOERS_CONFIG_DIR="/usr/etc/sudoers.d"
+SUDOERS_CONFIG_CUSTOM_PATH="$SUDOERS_CONFIG_DIR/99-bootstrap-scripts"
 
 # install essential tools
 sudo zypper --non-interactive --quiet install curl jq git tig
@@ -12,3 +14,16 @@ cd $WORKDIR
 git clone https://github.com/krsmanovic/dotfiles.git
 rsync -a dotfiles/opensuse-tumbleweed/ /home/$DESKTOP_USER/
 sudo chown -R $DESKTOP_USER:$DESKTOP_USER /home/$DESKTOP_USER/
+
+# allow config scripts to mess up the system
+sudo visudo --check
+if sudo grep "@includedir $SUDOERS_CONFIG_DIR" /etc/sudoers; then
+    if [ -d $SUDOERS_CONFIG_DIR ]; then
+        echo
+    else
+        sudo mkdir -p $SUDOERS_CONFIG_DIR
+    fi
+    sudo tee $SUDOERS_CONFIG_CUSTOM_PATH > /dev/null << EOF
+$DESKTOP_USER ALL=(ALL:ALL) NOPASSWD:/home/$DESKTOP_USER/scripts/setup/01-bootstrap.sh
+EOF
+fi
