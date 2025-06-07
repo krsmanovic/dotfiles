ce5735b7a0d342f875c8d2e5953bab4e59025349
krsmanovic <cedo.krsmanovic@gmail.com>
2025-06-07 11:43:05 +0200

    [fix] sudoers mess

--- opensuse-tumbleweed/scripts/setup/00-clone.sh
+++ opensuse-tumbleweed/scripts/setup/00-clone.sh
@@ -16,8 +16,7 @@ rsync -a dotfiles/opensuse-tumbleweed/ /home/$DESKTOP_USER/
 sudo chown -R $DESKTOP_USER:$DESKTOP_USER /home/$DESKTOP_USER/
 
 # allow config scripts to mess up the system
-sudo visudo --check
-if sudo grep "@includedir $SUDOERS_CONFIG_DIR" /etc/sudoers; then
+if sudo grep "@includedir $SUDOERS_CONFIG_DIR" /usr/etc/sudoers; then
     if [ -d $SUDOERS_CONFIG_DIR ]; then
         echo
     else
@@ -27,3 +26,6 @@ if sudo grep "@includedir $SUDOERS_CONFIG_DIR" /etc/sudoers; then
 $DESKTOP_USER ALL=(ALL:ALL) NOPASSWD:/home/$DESKTOP_USER/scripts/setup/01-bootstrap.sh
 EOF
 fi
+# visudo --check suggests 440 permissions
+sudo chmod 440 /usr/etc/sudoers
+sudo chmod 440 $SUDOERS_CONFIG_CUSTOM_PATH
