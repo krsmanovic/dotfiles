d0f10ed642f12ee97e58d0682207ed05e9353366
Cedomir Krsmanovic <cedo.krsmanovic@gmail.com>
2025-10-25 20:31:49 +0200

    [add] placeholder for llamma.cpp

--- opensuse-tumbleweed/scripts/setup/01-bootstrap.sh
+++ opensuse-tumbleweed/scripts/setup/01-bootstrap.sh
@@ -311,6 +311,12 @@ fi
 log_message info "Please run command 'opi codecs' to install all relevant media libraries!"
 echo "Please run command 'opi codecs' to install all relevant media libraries!"
 
+# once i get gfx card with cuda
+# git clone https://github.com/ggml-org/llama.cpp
+# cmake llama.cpp -B llama.cpp/build -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON -DLLAMA_CURL=ON
+# cmake --build llama.cpp/build --config Release -j --clean-first --target llama-cli llama-gguf-split
+# cp llama.cpp/build/bin/llama-* llama.cpp
+
 # start packagekit
 if systemctl list-unit-files packagekit.service &>/dev/null; then
     sudo systemctl start --now packagekit
