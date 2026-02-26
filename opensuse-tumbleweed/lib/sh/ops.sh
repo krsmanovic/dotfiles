d6dc3f36eeb0027a505d924b0be15eb61445fdb7
Cedomir Krsmanovic <cedo.krsmanovic@gmail.com>
2026-02-26 09:41:08 +0100

    [add] rename mp3 function

--- opensuse-tumbleweed/lib/sh/ops.sh
+++ opensuse-tumbleweed/lib/sh/ops.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# operational functions that save me more than 5s of typing or reading man pages :)
+# operational functions that save me more than 5s of typing, thinking or reading man pages :)
 
 reinstall_flatpaks () {
     for pak in $(flatpak list --columns=application --user | grep -v "Application ID"); do
@@ -10,4 +10,18 @@ reinstall_flatpaks () {
 
 show_init () {
     ps -p 1 -o comm=
+}
+
+rename_mp3_1 () {
+    IFS=$'\n'
+    artist=""
+    if [ "$#" -gt 0 ]; then
+        artist="${@} - "
+    fi
+    for i in $(ls -1 *.mp3); do
+        new_name=$(echo -n "$i" | sed -r "s/([0-9]{2})[^a-zA-Z0-9]+(.*\.mp3)/$artist\1 - \2/")
+        mv $i $new_name
+        echo "$i > $new_name"
+    done
+    unset IFS
 }
